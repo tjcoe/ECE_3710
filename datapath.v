0dceeb5a4abc95d70a3fb227fbbbe2c70b86beb3
Isaac Stagg <ikestagg@gmail.com>
2022-11-08 14:48:01 -0700

    implemented pc logic

--- datapath.v
+++ datapath.v
@@ -1,12 +1,12 @@
-module datapath (input clk, reset, pcEn, instrWrite, regWrite, writeBackSelect, dataToWriteSelect, pcSrc, newAluInput,
-                 input [1:0] aluSrc1Select, aluSrc2Select,
+module datapath (input clk, reset, pcEn, instrWrite, regWrite, writeBackSelect, dataToWriteSelect, newAluInput,
+                 input [1:0] aluSrc1Select, aluSrc2Select, pcSrc,
                  input [15:0] memDataInbound,
                  output aluOutIsZero,
                  output [7:0] PSR,
                  output [15:0] instr, memWriteData, memAddr, pcAddr);
 
     wire [3:0]  regDest, regSrc;
-    wire [15:0] aluResult, nextPc, currentPc, regDataA, regDataB, a, b, aluSrc1, aluSrc2, writeBackData, writeBack, writeDataRF, storedMemData, immediate;
+    wire [15:0] aluResult, nextPc, currentPc, regDataA, regDataB, a, b, aluSrc1, aluSrc2, writeBackData, writeBack, writeDataRF, storedMemData, immediate, incrPc, branchPc;
 
     // register file address fields
     assign regDest = instr[11:8];
@@ -28,7 +28,7 @@ module datapath (input clk, reset, pcEn, instrWrite, regWrite, writeBackSelect, 
 
     mux2 writeDataMux(.d0(writeBackData), .d1(currentPc), .sel(dataToWriteSelect), .res(writeDataRF));
     mux2 writeBackMux(.d0(aluResult), .d1(storedMemData), .sel(writeBackSelect), .res(writeBack));
-    mux2 pcMux(.d0(aluResult), .d1(b), .sel(pcSrc), .res(nextPc));
+    mux4 pcMux(.d0(incrPc), .d1(b), .d2(branchPc), .d3(16'b0), .sel(pcSrc), .res(nextPc));
     mux4 aluSrc1Mux(.d0(currentPc), .d1(a), .d2(16'b0), .d3(16'b1), .sel(aluSrc1Select), .res(aluSrc1));
     mux4 aluSrc2Mux(.d0(b), .d1(immediate), .d2(16'b1), .d3(16'b0), .sel(aluSrc2Select), .res(aluSrc2));
 
@@ -36,6 +36,6 @@ module datapath (input clk, reset, pcEn, instrWrite, regWrite, writeBackSelect, 
     registerfile rf(.clk(clk), .writeen(regWrite), .readaddrA(regDest), .readaddrB(regSrc), .writedata(writeDataRF), .outdataA(regDataA), .outdataB(regDataB));
     alu          alunit(.a(aluSrc1), .b(aluSrc2), .opCode(instr[15:12]), .opExt(instr[7:4]), .PSR(PSR), .result(aluResult));
     zerodetect   zd(.a(aluResult), .y(aluOutIsZero));
-    //SimpleAdder  Disp(.a(), .b(), .c());
-    //SimpleAdder  PcIncr();
+    SimpleAdder  Disp(.a(currentPc), .b(16'b1), .s(0), .c(incrPc));
+    SimpleAdder  PcIncr(.a(currentPc), .b(immediate), .s(1), .c(branchPc));
 endmodule
