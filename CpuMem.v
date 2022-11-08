f3cca6d1ce90802617018dc859c850e2a5920002
Isaac Stagg <ikestagg@gmail.com>
2022-11-08 13:45:36 -0700

    added dual port functionality

--- CpuMem.v
+++ CpuMem.v
@@ -1,9 +1,9 @@
 module CpuMem(input clk, reset);
     wire [15:0] memDataA, addrA, writeDataA, memDataB, addrB, WriteDataB;
-    wire we;
+    wire weA, weB;
 
     cpu proc(.clk(clk), .reset(reset), .memDataInbound(memDataA),
              .memWrite(we), .memWriteData(writeDataA), .memAddr(addrA));
-    memory ram_block(.a_address(addrA), .b_address(addrB), .a_writeData(writeDataA), .b_writeData(WriteDataB), .a_we(we), .b_we(), .clk(clk),
+    memory ram_block(.a_address(addrA), .b_address(addrB), .a_writeData(writeDataA), .b_writeData(WriteDataB), .a_we(weA), .b_we(weB), .clk(clk),
                      .a_out(memDataA), .b_out(memDataB));
 endmodule
