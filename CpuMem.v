0935b607c3ed0ee0f3fd3d5143c3cdb519e24146
Isaac Stagg <ikestagg@gmail.com>
2022-11-08 17:22:49 -0700

    fixed for pc

--- CpuMem.v
+++ CpuMem.v
@@ -2,8 +2,8 @@ module CpuMem(input clk, reset);
     wire [15:0] memDataA, addrA, writeDataA, memDataB, addrB, WriteDataB;
     wire weA, weB;
 
-    cpu proc(.clk(clk), .reset(reset), .memDataInbound(memDataA),
-             .memWrite(we), .memWriteData(writeDataA), .memAddr(addrA));
-    memory ram_block(.a_address(addrA), .b_address(addrB), .a_writeData(writeDataA), .b_writeData(WriteDataB), .a_we(weA), .b_we(weB), .clk(clk),
-                     .a_out(memDataA), .b_out(memDataB));
+    cpu proc(.clk(clk), .reset(reset), .memDataInbound(memDataA), .pcData(memDataB),
+             .memWrite(we), .memWriteData(writeDataA), .memAddr(addrA), .pcAddr(addrB));
+    ram_block mem(.a_address(addrA[15:4]), .b_address(addrB[15:4]), .a_writeData(writeDataA), .b_writeData(WriteDataB), .a_we(weA), .b_we(weB), .clk(clk),
+                  .a_out(memDataA), .b_out(memDataB));
 endmodule
