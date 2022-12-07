ef595157f1ec80da863fb9d0c673dd66cec543f8
Nate Tripp <nate.tripp13448@gmail.com>
2022-12-07 16:42:32 -0700

    Finished ASM and added binary file

--- Paint.asm
+++ Paint.asm
@@ -14,7 +14,7 @@
 ;#define BLACK = 7
 ;
 ;int main() {
-;    short x = RED; // r4
+;    short color = RED; // r4
 ;    short SHORT_MAX = 0xFFFF; // r11
 ;
 ;    while(true) {
@@ -25,21 +25,21 @@
 ;        short yPos = MEM[loadAdd]; // r6
 ;
 ;        loadAdd = 4089
-;        short buttons = MEM[loadAdd]; // r7
+;        short buttons = MEM[loadAdd]; // r7 0000000LMR
 ;
 ;        // In color picker?
 ;        if (yPos < 80) { // In color picker
-;            short leftMouseClickMask = 4; // r8
+;            short leftMouseClickMask = 4; // r8 100
 ;            leftMouseClickMask = leftMouseClickMask & buttons;
 ;
 ;            if(leftMouseClickMask == 0) { // Left mouse not clicked
 ;                break;
 ;            }
 ;
-;            // Left button clicked, select color
+;            // Left button clicked select color
 ;            // Check the xPos to determine color
 ;            if(xPos <= 213) {
-;                color = RED; // Set color to red, and loop back.
+;                color = RED; // Set color to red and loop back.
 ;                break;
 ;            }
 ;            else {
@@ -78,13 +78,17 @@
 ;                }
 ;
 ;                // Divide both positions by 8 to get correct buffer index
-;                xPos = xPos >> 3;
-;                yPos = yPos >> 3;
-;
-;                yPos = xPos * yPos; // yPos now equals the 0 based buffer index
+;				 yPos = yPos - 80;
+;                xPos = xPos >> 3; // row   
+;                yPos = yPos >> 3; // column
+;				 
+;				 yPos = yPos * 80;
+;                yPos = xPos + yPos; // yPos now equals the 0 based buffer index
+;				 
 ;                short pixelIndex = yPos; // r10
 ;                pixelIndex = pixelIndex % 4; // Gets the pixel index out of 4 pixels
 ;                pixelIndex = pixelIndex << 2; // Get bit location of first bit in pixel
+;					
 ;
 ;                short memAddress = yPos; // MemAddress r8
 ;                memAddress = memAddress >> 2; MemAddress offset in memory buffer
@@ -92,7 +96,7 @@
 ;
 ;
 ;                short pixels = MEM[memAddress]; // r9 Load the 4 pixels in memory line;
-;                xPos = 0xF;               // r5 Create mask to zero out pixel that we are going to set
+;                xPos = 0xF;               		// r5 Create mask to zero out pixel that we are going to set
 ;                xPos = xPos << pixelIndex;      // Shift mask to cover pixel to set.
 ;
 ;                xPos = xPos ^ SHORT_MAX;        // Leaves xPos as all ones except where we zeroed out the pixel.
@@ -112,45 +116,56 @@
 
 
 Main: 
-    MOVI $1, %r4 ; Initialize the color to Red
-    MOVI $65535, %r11   ; %r11 = SHORT MAX
+    MOVI $1 %r4    ; Initialize the color to Red
+    MOV %r0 %r11   ; Initialize r11 to zero. 
+    SUBI $1 %r11   ; %r11 = SHORT MAX
+    MOVI $4 %r3    ; Store loop start
+    MOVI $47 %r14  ; Store UpdateColor
 
 Loop: 
-    MOVI $4088, %r13 ; 4088 contains x position
-    LOAD %r13, %r5
-    MOVI $4087, %r13 ; 4087 contains y position
-    LOAD %r13, %r6
-    MOVI $4089, %r13 ; 4089 contains buttons
-    LOAD %r13, %r7
+    MOVI $1 %r13   ; Move 1 into load address register
+    LSHI $12 %r13  ; r13 now equals 4096
+
+
+    SUBI $7 %r13   ; 4089 contains buttons
+    LOAD %r13 %r7
+    SUBI $1 %r13   ; 4088 contains x position
+    LOAD %r13 %r5
+    SUBI $1 %r13   ; 4087 contains y position
+    LOAD %r13 %r6
+
 
     ; In color picker?
-    CMPI $80, %r6 ; Check to see if we are in the canvas or control space
-    BGE InCanvas
+    CMPI $80 %r6 ; Check to see if we are in the canvas or control space
+    BGE $18    ;Should be 18
 
-    MOVI $4, %r8    ;  Set r8 to 0b100
-    AND %r7, %r8    ;  Check to see if left mouse button is clicked
-    CMPI %r8, %r0   
-    BEQ Loop    ; Left mouse button is not pressed, loop again
+    MOVI $4 %r8    ;  Set r8 to 0b100
+    AND %r7 %r8    ;  Check to see if left mouse button is clicked
+    CMP %r8 %r0   
+    JEQ %r3    ; Left mouse button is not pressed loop again
 
     ; Else figure out what color we are in
 
 CheckRed:
-    CMPI $213, %r5  ; Red is at xpos <= 213
-    BG CheckGreen   ; Jump to green check
-    MOVI $1, %r4    ; No jump, set to red
-    Jump Loop       ; New color selected, loop again
+    MOVI $127 %r1  ; Move 127 into r1 we need R1 to equal 213
+    ADDI $86 %r1  ; Move 127 into r1 we need R1 to equal 213
+    CMP %r1 %r5  ; Red is at xpos <= 213
+    BGT $3   ; Jump to green check
+    MOVI $1 %r4    ; No jump set to red
+    JUC %r3       ; New color selected loop again
 
 CheckGreen:
-    CMPI $426, %r5  ; Green is at xpos 214 <= x <= 426
-    BG CheckBlue    ; Jump to blue check
-    MOVI $2, %r4    ; No jump, set to red
-    Jump Loop       ; New color selected, loop again
+    LSHI $1 %r1    ; Get r1 to equal 426
+    CMP %r1 %r5  ; Green is at xpos 214 <= x <= 426
+    BGT $3      ; Jump to blue check
+    MOVI $2 %r4    ; No jump set to red
+    JUC %r3       ; New color selected loop again
 
 CheckBlue:
-    MOVI $4, %r4    ; Default set to blue
-    Jump Loop       ; New color selected, loop again
+    MOVI $4 %r4    ; Default set to blue
+    JUC %r3       ; New color selected loop again
 
-; Buffer = (400 * 640) / 8 = 32,000
+; Buffer = (400 * 640) / 8 = 32000
 ; Buffer[0]     = 0x4000 = 16384
 ; Buffer[1]     = 0x4004 = 16388
 ; Buffer[2]     = 0x4008 = 16392
@@ -158,47 +173,55 @@ CheckBlue:
 ; Buffer[7999]  = 0x7D00 = 32000
 
 InCanvas:
-    CMP %r0, %r7    ; Check if mouse is clicked
-    BEQ Loop        ; No buttons clicked, loop
+    CMP %r0 %r7    ; Check if mouse is clicked
+    JUC %r3        ; No buttons clicked loop
 
 SetColorWhite:
-    MOV %r7, %r1            ; Copy buttons into temp
-    ANDI $1, %r1            
-    CMPI $1, %r1            ; Is rmb clicked?
-    BNE CheckMiddleMouse
-    MOVI $0, %r2            ; Color equals 0 (white)
-    Jump UpdateColor
+    MOV %r7 %r1            ; Copy buttons into temp
+    ANDI $1 %r1            
+    CMPI $1 %r1            ; Is rmb clicked?
+    BNE $3                  ; To CheckMiddleMouse
+    MOVI $0 %r2            ; Color equals 0 (white)
+    JUC %r14
 
 CheckMiddleMouse:
-    MOV %r7, %r1            ; Copy buttons into temp
-    ANDI $2, %r1   
-    CMPI $2, %r1            ; Is rmb clicked?
-    BNE LeftMouseClicked
-    MOVI $7, %r2            ; Color equals 7 (black)
-    Jump UpdateColor
+    MOV %r7 %r1            ; Copy buttons into temp
+    ANDI $2 %r1   
+    CMPI $2 %r1            ; Is rmb clicked?
+    BNE $3                  ; To LeftMouseClicked
+    MOVI $7 %r2            ; Color equals 7 (black)
+    JUC %r14
 
 LeftMouseClicked:
-    MOV %r4, %r2
+    MOV %r4 %r2
 
 UpdateColor:
-    RSHI $3, %r5    ; Divide position by 8 to get buffer address
-    RSHI $3, %r6    ; Divide position by 8 to get buffer address
-    MUL %r5, %r6    ; Multiple to get buffer address in r6
-    MOV %r6, %r10   ; Store buffer offset
-    MODI $4, %r10   ; Get pixel location in memory address
-    LSHI $2, %r10   ; Get bit location of first bit in pixel
+    RSHI $3 %r5    ; Should be 47 Divide position by 8 to get buffer address
+    SUBI $80 %r6   ; Subtract off control space to get 0 based index
+    RSHI $3 %r6    ; Divide position by 8 to get buffer address
 
-    MOV %r6, %r8    ; Create temp variable in $r8
-    RSHI $2, %r8    ; Divide temp by 4 to get offset
-    ADDI $1024, %r8 ; Add 1024 to get into MemLocation
+    MOVI $80 %r1   ;
+    MUL %r1 %r6    ; Set yPos (r6) to be the correct row offset
+    ADD %r5 %r6    ; Add to get buffer address in r6
+    
+    MOV %r6 %r10   ; Store buffer offset
+    MODI $4 %r10   ; Get pixel location in memory address
+    LSHI $2 %r10   ; Get bit location of first bit in pixel
+
+    MOV %r6 %r8    ; Create temp variable in $r8
+    RSHI $2 %r8    ; Divide temp by 4 to get offset
+
+    MOVI $1 %r12   ; Move 1 into r12
+    LSHI $10 %r12  ; r12 = 1024
+    ADD %r12 %r8   ; Add 1024 to get into MemLocation
                     
-    LOAD %r8, %r9       ; Load pixel from buf location
-    MOVI $15, %r5       ; r5 = 0b1111
-    LSH %r10, %r5       ; shift 1111 to 0 out r11
-    XOR %r11, %r5       ; %r5 = 1's where the pixel is not
-    AND %r5, %r9        ; 0 out pixels to be written to 
-    LSH %r10, %r2       ; shift pixel value to where it needs to be
-    OR %r2, %r9         ; r9 pixel color in position
-    STORE %r8, %r9      ; pixel written
+    LOAD %r8 %r9       ; Load pixel from buf location
+    MOVI $15 %r5       ; r5 = 0b1111
+    LSH %r10 %r5       ; shift 1111 to 0 out r11
+    XOR %r11 %r5       ; %r5 = 1's where the pixel is not
+    AND %r5 %r9        ; 0 out pixels to be written to 
+    LSH %r10 %r2       ; shift pixel value to where it needs to be
+    OR %r2 %r9         ; r9 pixel color in position
+    STOR %r8 %r9       ; pixel written
 
-    Jump Loop
+    JUC %r3
